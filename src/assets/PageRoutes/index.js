dc1aa0a1bc15c2848f4f09cb78c173e9b37d47d6
EmersonSousa15 <emersonsousac14@gmail.com>
2022-12-13 10:48:41 -0300

    Header completo

--- src/assets/PageRoutes/index.js
+++ src/assets/PageRoutes/index.js
@@ -2,12 +2,16 @@ import {Routes, Route} from 'react-router-dom';
 
 import Home from '../../pages/Home';
 import Projects from '../../pages/Projects';
+import Empresa from '../../pages/Empresa';
+import Contato from '../../pages/Contato';
 
 const PageRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/projects' element={<Projects/>} />
+            <Route path='/company' element={<Empresa/>} />
+            <Route path='/contact' element={<Contato/>} />
         </Routes>
     )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,20 @@
 import {BsCoin} from 'react-icons/bs';
-
 import {Link} from 'react-router-dom';
+
+import './style.css';
 
 const Header = () => {
     return (
         <header>
-            <BsCoin />
+            <div className='icon--header'>
+                <BsCoin />
+            </div>
 
-            <ul>
+            <ul className='menu'>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/projects'>Projetos</Link></li>
+                <li><Link to='/company'>Empresa</Link></li>
+                <li><Link to='/contact'>Contato</Link></li>
             </ul>
         </header>
     )
